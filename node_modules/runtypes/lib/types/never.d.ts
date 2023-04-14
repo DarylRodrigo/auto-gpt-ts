7b6ed94809f4bcfb509abe986e1d90d19a194661
Daryl Rodrigo <daryl@orkestro.com>
2023-04-14 07:06:36 +0100

    Added git ignore

--- node_modules/runtypes/lib/types/never.d.ts
+++
@@ -1,8 +0,0 @@
-import { Runtype } from '../runtype';
-export interface Never extends Runtype<never> {
-    tag: 'never';
-}
-/**
- * Validates nothing (unknown fails).
- */
-export declare const Never: Never;
--- node_modules/runtypes/lib/types/void.d.ts
+++
@@ -1,8 +0,0 @@
-import { Unknown } from './unknown';
-export declare type Void = Unknown;
-/**
- * Void is an alias for Unknown
- *
- * @deprecated Please use Unknown instead
- */
-export declare const Void: Unknown;
